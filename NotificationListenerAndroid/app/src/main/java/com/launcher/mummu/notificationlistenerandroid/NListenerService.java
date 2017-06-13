611b1e4c55794c5ac9dc47bd1a2c9008f648a61e
muhammed <muhammed.thasneem@yahoo.com>
2017-06-13 14:21:21 +0530

    checking whatsapp package

--- NotificationListenerAndroid/app/src/main/java/com/launcher/mummu/notificationlistenerandroid/NListenerService.java
+++ NotificationListenerAndroid/app/src/main/java/com/launcher/mummu/notificationlistenerandroid/NListenerService.java
@@ -1,23 +1,28 @@
 package com.launcher.mummu.notificationlistenerandroid;
 
-import android.annotation.SuppressLint;
-import android.os.Build;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 /**
  * Created by muhammed on 2/15/2017.
  */
 
-@SuppressLint("OverrideAbstract")
-@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+
 public class NListenerService extends NotificationListenerService {
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         String packageName = sbn.getNotification().tickerText.toString();
-        Log.d("NOTIFICATION_LISTENER", "onNotificationPosted: " + packageName);
+        if (sbn.getPackageName().equalsIgnoreCase("com.whatsapp")) {
+            Log.d("NOTIFICATION_LISTENER", "onNotificationPosted: Whatsapp : " + packageName);
+        } else {
+            Log.d("NOTIFICATION_LISTENER", "onNotificationPosted : " + packageName);
+        }
         super.onNotificationPosted(sbn);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+
     }
 }
